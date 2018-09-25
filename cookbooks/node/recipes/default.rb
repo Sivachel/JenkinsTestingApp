d8bbdbe2b0dd2feff11b47b5e273ab8464e4bec9
Sivachel <lsivachelvan@spartaglobal.com>
2018-09-25 17:35:18 +0100

    added packer.json and packermongod.json

---
+++ cookbooks/node/recipes/default.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook:: node
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+apt_update 'update_sources' do
+  action :update
+end
+
+include_recipe('nodejs')
+nodejs_npm ("pm2") do
+  action :install
+end
+
+package "nginx" do
+  action :install
+end
+
+template '/etc/nginx/sites-available/proxy.conf' do
+  source 'proxy.conf.erb'
+  notifies(:restart, 'service[nginx]')
+end
+
+link '/etc/nginx/sites-enabled/proxy.conf' do
+  to '/etc/nginx/sites-available/proxy.conf'
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+  notifies(:restart, 'service[nginx]')
+end
+
+service "nginx" do
+  action [:enable, :start]
+end
